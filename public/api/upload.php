5005c502a14652a78bb3b9519d7ff43014192d9d
HOPOLOK <vadim.truhanov@inbox.lv>
2025-10-23 00:07:21 +0300

    file was uploaded

--- public/api/upload.php
+++ public/api/upload.php
@@ -1,5 +1,7 @@
 <?php
 
+define("FILE_MAXSIZE", 2_000_000); // 2 mb
+
 session_start();
 
 if (empty($_SESSION['auth'])) {
@@ -8,4 +10,32 @@ if (empty($_SESSION['auth'])) {
     exit;
 }
 
-echo "uploading file bruv";
+if (empty($_FILES['file'])) {
+    http_response_code(400);
+    header("Location: /");
+    exit;
+}
+
+if ($_FILES['file']['size'] > FILE_MAXSIZE) {
+    http_response_code(400);
+    header("Location: /");
+    exit;
+}
+
+// TODO - file name validation
+
+require_once "../../private/db/file.php";
+if (file_check($_SESSION['username'], $_FILES['file']['name'])) {
+    http_response_code(400);
+    header("Location: /");
+    exit;
+}
+
+file_upload($_SESSION['username'], $_FILES['file']['name']);
+
+mkdir("../../uploads/".$_SESSION['username']);
+move_uploaded_file($_FILES['file']['tmp_name'], "../../uploads/".$_SESSION['username']."/".$_FILES['file']['name']);
+
+http_response_code(303);
+header("Location: /");
+exit;
